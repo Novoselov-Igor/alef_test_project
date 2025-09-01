a059cde9dc93eb114b19d8589539937b682785f1
Igor <barszx23@gmail.com>
2025-09-01 19:13:11 +0400

    Add migrations

--- database/migrations/2025_08_29_100341_create_class_lecture_plans_table.php
+++ database/migrations/2025_08_29_100341_create_class_lecture_plans_table.php
@@ -4,17 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('class_lecture_plans', function (Blueprint $table) {
+        Schema::create('class_lecture_plan', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('school_class_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('lecture_id')->constrained()->cascadeOnDelete();
+            $table->unsignedInteger('order');
+            $table->timestamp('completed_at')->nullable()->after('order');
             $table->timestamps();
+
+            // Один и тот же урок не может быть дважды в плане одного класса
+            $table->unique(['school_class_id', 'lecture_id']);
+            $table->index(['school_class_id', 'order']);
         });
+
     }
 
     /**
